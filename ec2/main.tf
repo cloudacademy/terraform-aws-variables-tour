a08db0f5ea6266bd4cda5798caaf4f2d7fc9a500
Jun <jun.fritz@gmail.com>
2023-11-14 17:15:19 -1000

    starting point

---
+++ ec2/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.8.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
+
